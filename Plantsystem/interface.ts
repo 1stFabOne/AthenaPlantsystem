a0746e2e522ee116603c4b352c922573417a2cfb
Der Lord! <82890183+Booster1212@users.noreply.github.com>
2021-09-05 08:13:51 +0200

    Fertilizer, Water .. some fixes.
    - Commit Fertilizing & Watersystem.

---
+++ Plantsystem/interface.ts
@@ -0,0 +1,327 @@
+/************************************************************
+ *      PLANT-System for ATHENA-Framework by Der Lord!      *
+ * Explanations can be found in this across the source code *      
+ ************************************************************/
+import * as alt from 'alt-server';
+import Database from "@stuyk/ezmongodb";
+import { TextLabelController } from '../../server/systems/textlabel';
+import { ObjectController } from '../../server/systems/object';
+import { distance2d } from '../../shared/utility/vector';
+import { playerFuncs } from '../../server/extensions/Player';
+import { getFromRegistry } from '../../shared/items/itemRegistry';
+
+const smallPlant = 'bkr_prop_weed_01_small_01a';
+const mediumPlant = 'bkr_prop_weed_med_01a';
+const bigPlant = 'bkr_prop_weed_lrg_01a';
+
+const harvestDuration = 5000; // Duration for Harvesting in MS
+
+const fertilizeDuration = 5000; // Duration for Fertilizing in MS
+const fertilizerRemoveTime = 30; // How many minutes will the fertilizer remove?
+
+const wateringDuration = 5000; // Duration for Watering in MS
+const minRequiredWater = 50; // How much water is minimum needed for the plant to grow?
+
+const minBuds = 25; // Min output as weedbuds from harvesting
+const maxBuds = 50; // Maximum output as weedbuds from harvesting
+
+// Plants-Interface
+interface Plants {
+    _id?: any,
+    plantOwner?: string,
+    position: {
+        x: number,
+        y: number,
+        z: number
+    }
+    data: {
+        state: string,
+        remainingMinutes: number,
+        water: number,
+        fertilizer: any,
+        isHarvestable: any
+    }
+}
+
+// Build a new Plant with a Small-Pot
+export async function buildPlant(plantOwner: string, posX: number, posY: number, posZ: number, water, fertilizer, state: string, remainingMinutes: number, isHarvestable: any) {
+    const newDocument = {
+        plantOwner: plantOwner,
+        position: {
+            x: posX,
+            y: posY,
+            z: posZ
+        },
+        data: {
+            state: state,
+            remainingMinutes: remainingMinutes,
+            water: water,
+            fertilizer: fertilizer,
+            isHarvestable: isHarvestable
+        }
+    }
+    const plants = await Database.insertData<Plants>(newDocument, 'plants', true);
+    if (!plants) {
+        throw new Error('Could not insert data for table <plants>');
+    }
+
+    ObjectController.append({
+        model: smallPlant,
+        uid: `WeedPlantIdentifier-${plants._id}`,
+        pos: plants.position
+    });
+
+    alt.log(`${plantOwner} placed a Weedpot at ${posX}, ${posY}, ${posZ}!`);
+    updateAllPlants();
+};
+
+export async function loadAllExistingPlants() {
+    const allPlants = await Database.fetchAllData<Plants>('plants');
+    if (!allPlants) {
+        throw new Error('Could not fetch data for table <plants>');
+    }
+    allPlants.forEach(plant => {
+        if (plant.data.remainingMinutes > 45) {
+            ObjectController.append({
+                model: smallPlant,
+                uid: `WeedPlantIdentifier-${plant._id}`,
+                pos: plant.position
+            });
+        } else if (plant.data.remainingMinutes <= 45 && plant.data.remainingMinutes > 15) {
+            ObjectController.append({
+                model: mediumPlant,
+                uid: `WeedPlantIdentifier-${plant._id}`,
+                pos: plant.position
+            });
+        } else if (plant.data.remainingMinutes <= 0) {
+            ObjectController.append({
+                model: bigPlant,
+                uid: `WeedPlantIdentifier-${plant._id}`,
+                pos: plant.position
+            });
+        }
+
+        TextLabelController.append({
+            uid: `TextLabelIdentifier-${plant._id}`,
+            pos: { "x": plant.position.x, "y": plant.position.y, "z": plant.position.z + 0.5 },
+            data: `~w~State: ~g~${plant.data.state}~n~~w~Fertilizer: ~g~${plant.data.fertilizer}~n~~w~Water: ~g~${plant.data.water}%~n~~w~Time: ~g~${plant.data.remainingMinutes} ~w~minutes`,
+            maxDistance: 10
+        });
+
+        if (plant.data.remainingMinutes <= 0) {
+            plant.data.remainingMinutes = 0;
+            TextLabelController.remove(`TextLabelIdentifier-${plant._id}`);
+            TextLabelController.append({
+                uid: `TextLabelIdentifier-${plant._id}`,
+                pos: { "x": plant.position.x, "y": plant.position.y, "z": plant.position.z + 0.5 },
+                data: `~g~Harvestable`,
+                maxDistance: 10
+            });
+        }
+    });
+    alt.log(`Found ${allPlants.length} plants to load in the database.`);
+}
+
+export async function updateAllPlants() {
+    const allPlants = await Database.fetchAllData<Plants>('plants');
+    if (!allPlants) {
+        throw new Error('Could not fetch data for table <plants>');
+    }
+    allPlants.forEach(async plant => {
+        const TextLabelIdentifier = `TextLabelIdentifier-${plant._id}`;
+
+        if (plant.data.remainingMinutes >= 0) {
+            TextLabelController.remove(`TextLabelIdentifier-${plant._id}`);
+            TextLabelController.append({
+                uid: TextLabelIdentifier,
+                pos: { "x": plant.position.x, "y": plant.position.y, "z": plant.position.z + 0.5 },
+                data: `~w~State: ~g~${plant.data.state}~n~~w~Fertilizer: ~g~${plant.data.fertilizer}~n~~w~Water: ~g~${plant.data.water}%~n~~w~Time: ~g~${plant.data.remainingMinutes} ~w~minutes`,
+                maxDistance: 10
+            });
+
+            if (plant.data.remainingMinutes == 45) {
+                plant.data.state = 'Growthphase...';
+                alt.setTimeout(() => {
+                    ObjectController.remove(`WeedPlantIdentifier-${plant._id}`);
+                    ObjectController.append({
+                        model: mediumPlant,
+                        uid: `WeedPlantIdentifier-${plant._id}`,
+                        pos: plant.position
+                    });
+
+                    TextLabelController.remove(`TextLabelIdentifier-${plant._id}`);
+                    TextLabelController.append({
+                        uid: TextLabelIdentifier,
+                        pos: { "x": plant.position.x, "y": plant.position.y, "z": plant.position.z + 0.5 },
+                        data: `~w~State: ~g~${plant.data.state}~n~~w~Fertilizer: ~g~${plant.data.fertilizer}~n~~w~Water: ~g~${plant.data.water}%~n~~w~Time: ~g~${plant.data.remainingMinutes} ~w~minutes`,
+                        maxDistance: 10
+                    });
+                }, 250);
+            }
+
+            if (plant.data.remainingMinutes == 15) {
+                plant.data.state = 'Endphase...';
+                alt.setTimeout(() => {
+                    ObjectController.remove(`WeedPlantIdentifier-${plant._id}`);
+                    ObjectController.append({
+                        model: bigPlant,
+                        uid: `WeedPlantIdentifier-${plant._id}`,
+                        pos: plant.position
+                    });
+                }, 250);
+            }
+
+            if (plant && plant.data.remainingMinutes == 0 && plant.data.isHarvestable == false) {
+                plant.data.isHarvestable = true;
+                plant.data.remainingMinutes = 0;
+                TextLabelController.remove(`TextLabelIdentifier-${plant._id}`);
+                TextLabelController.append({
+                    uid: `TextLabelIdentifier-${plant._id}`,
+                    pos: { "x": plant.position.x, "y": plant.position.y, "z": plant.position.z + 0.5 },
+                    data: `~g~Harvestable`,
+                    maxDistance: 10
+                });
+            }
+            if (plant.data.water < minRequiredWater) return;
+            // Updating Database Stuff..
+            if (plant.data.remainingMinutes >= 0) {
+                await Database.updatePartialData(plant._id, {
+                    data:
+                    {
+                        state: plant.data.state,
+                        remainingMinutes: plant.data.remainingMinutes -= 1,
+                        water: plant.data.water,
+                        fertilizer: plant.data.fertilizer,
+                        isHarvestable: plant.data.isHarvestable
+                    }
+                }, 'plants');
+            }
+        }
+    });
+}
+async function harvestPot(player: alt.Player) {
+    const allPlants = await Database.fetchAllData<Plants>('plants');
+    if (!allPlants) {
+        throw new Error('Could not fetch data for table <plants>');
+    }
+    allPlants.forEach(async plants => {
+        if (distance2d(player.pos, plants.position) <= 1) {
+            if (plants.data.fertilizer) {
+                playerFuncs.emit.notification(player, "Plant is already fertilized.");
+                return;
+            }
+            if (!plants.data.isHarvestable) {
+                playerFuncs.emit.notification(player, "This plant is not harvestable yet.");
+                return;
+            }
+            playerFuncs.emit.scenario(player, "WORLD_HUMAN_GARDENER_PLANT", harvestDuration);
+            alt.setTimeout(() => {
+                TextLabelController.remove(`TextLabelIdentifier-${plants._id}`);
+                ObjectController.remove(`WeedPlantIdentifier-${plants._id}`);
+                alt.emit('Plantsystem:HandleGiveItems', player);
+            }, harvestDuration);
+            await Database.deleteById(plants._id, 'plants');
+        }
+    });
+};
+
+async function fertilizePot(player: alt.Player) {
+    const allPlants = await Database.fetchAllData<Plants>('plants');
+    if (!allPlants) {
+        throw new Error('Could not fetch data for table <plants>');
+    }
+    allPlants.forEach(async plant => {
+        if (plant.data.fertilizer) {
+            playerFuncs.emit.notification(player, "You can't fertilize a plant that's already fertilized.");
+            return;
+        }
+        if (distance2d(player.pos, plant.position) <= 1) {
+            playerFuncs.emit.scenario(player, "WORLD_HUMAN_GARDENER_LEAF_BLOWER", fertilizeDuration);
+            alt.setTimeout(async () => {
+                playerFuncs.emit.notification(player, `Successfully fertilized the plant infront of you.`);
+                await Database.updatePartialData(plant._id, {
+                    data:
+                    {
+                        state: plant.data.state,
+                        remainingMinutes: plant.data.remainingMinutes -= fertilizerRemoveTime,
+                        water: plant.data.water,
+                        fertilizer: true,
+                        isHarvestable: plant.data.isHarvestable
+                    }
+                }, 'plants');
+                updateAllPlants();
+            }, fertilizeDuration);
+        }
+    });
+}
+
+async function waterPot(player: alt.Player) {
+    const plantWater = getFromRegistry("plantwater");
+
+    const allPlants = await Database.fetchAllData<Plants>('plants');
+    if (!allPlants) {
+        throw new Error('Could not fetch data for table <plants>');
+    }
+
+    allPlants.forEach(async plant => {
+        if (plant.data.water >= 100) {
+            playerFuncs.emit.notification(player, "This plant has already maximum water.");
+            console.log(plant.data.water);
+            return;
+        } else {
+            if (distance2d(player.pos, plant.position) <= 1) {
+                alt.setTimeout(async () => {
+                    playerFuncs.emit.scenario(player, "WORLD_HUMAN_GARDENER_LEAF_BLOWER", wateringDuration);
+                    await Database.updatePartialData(plant._id, {
+                        data:
+                        {
+                            state: plant.data.state,
+                            remainingMinutes: plant.data.remainingMinutes,
+                            water: plant.data.water += plantWater.data.amount,
+                            fertilizer: plant.data.fertilizer,
+                            isHarvestable: plant.data.isHarvestable
+                        }
+                    }, 'plants');
+                    updateAllPlants();
+                }, wateringDuration);
+            }
+        }
+    });
+}
+
+function getRandomMinMax(min: number, max: number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+alt.on('Plantsystem:HarvestPot', (player: alt.Player) => {
+    harvestPot(player);
+});
+
+alt.on('Plantsystem:FertilizePot', (player: alt.Player) => {
+    fertilizePot(player);
+});
+
+alt.on('Plantsystem:WaterPot', (player: alt.Player) => {
+    waterPot(player);
+});
+
+alt.on('Plantsystem:HandleGiveItems', (player: alt.Player) => {
+    const itemFromRegistry = getFromRegistry("buds");
+    const itemExists = playerFuncs.inventory.isInInventory(player, { name: itemFromRegistry.name });
+    const emptySlot = playerFuncs.inventory.getFreeInventorySlot(player);
+
+    itemFromRegistry.quantity = getRandomMinMax(minBuds, maxBuds);
+    if (!itemExists) {
+        playerFuncs.emit.notification(player, `You receive ${itemFromRegistry.quantity} Weedbuds!`);
+        playerFuncs.inventory.inventoryAdd(player, itemFromRegistry, emptySlot.slot, emptySlot.tab);
+    } else {
+        playerFuncs.emit.notification(player, `You receive ${itemFromRegistry.quantity} Weedbuds!`);
+        player.data.inventory[itemExists.tab][itemExists.index].quantity += itemFromRegistry.quantity;
+    }
+
+    playerFuncs.save.field(player, 'inventory', player.data.inventory);
+    playerFuncs.save.field(player, 'toolbar', player.data.toolbar);
+    playerFuncs.sync.inventory(player);
+});
---
+++ Plantsystem/items.ts
@@ -0,0 +1,100 @@
+/************************************************************
+ *      PLANT-System for ATHENA-Framework by Der Lord!      *
+ * Explanations can be found in this across the source code *      
+ ************************************************************/
+import { ITEM_TYPE } from '../../shared/enums/itemTypes';
+import { Item } from "../../shared/interfaces/Item";
+import { appendToItemRegistry } from '../../shared/items/itemRegistry';
+import { deepCloneObject } from '../../shared/utility/deepCopy';
+
+const weedPotItem: Item = {
+    name: 'Weed Pot',
+    description: 'DEBUG: Change me! :( - I am used to plant a WeedPot.',
+    icon: 'crate',
+    slot: 0,
+    quantity: 15,
+    behavior:
+        ITEM_TYPE.CAN_DROP |
+        ITEM_TYPE.CAN_STACK |
+        ITEM_TYPE.CONSUMABLE |
+        ITEM_TYPE.IS_TOOLBAR,
+    data: {
+        event: 'Plantsystem:PlacePot',
+    }
+}
+const registerWeedPot: Item = deepCloneObject<Item>(weedPotItem);
+appendToItemRegistry(registerWeedPot);
+const weedBudsItem: Item = {
+    name: 'Buds',
+    description: 'DEBUG: Change me :( - I am the return of harvesting a WeedPot.',
+    icon: 'crate',
+    slot: 0,
+    quantity: 0,
+    behavior:
+        ITEM_TYPE.CAN_DROP |
+        ITEM_TYPE.CAN_STACK |
+        ITEM_TYPE.CONSUMABLE |
+        ITEM_TYPE.IS_TOOLBAR,
+    data: {
+        // None for now.
+    }
+}
+const registerWeedBuds: Item = deepCloneObject<Item>(weedBudsItem);
+appendToItemRegistry(registerWeedBuds);
+
+const secateursItem: Item = {
+    name: 'secateurs',
+    description: 'DEBUG: Change me :( - I am your little harvest helper to harvest WeedPots.',
+    icon: 'crate',
+    slot: 0,
+    quantity: 100,
+    behavior:
+        ITEM_TYPE.CAN_DROP |
+        ITEM_TYPE.CAN_STACK |
+        ITEM_TYPE.CONSUMABLE |
+        ITEM_TYPE.IS_TOOLBAR,
+    data: {
+        event: 'Plantsystem:HarvestPot',
+    }
+}
+const registerSecateurs: Item = deepCloneObject<Item>(secateursItem);
+appendToItemRegistry(registerSecateurs);
+
+const plantFertilizerItem: Item = {
+    name: 'Pot Fertilizer',
+    description: 'DEBUGItem.. Change me - I am used to fertilize WeedPots.',
+    icon: 'crate',
+    slot: 0,
+    quantity: 25,
+    behavior:
+        ITEM_TYPE.CAN_DROP |
+        ITEM_TYPE.CAN_STACK |
+        ITEM_TYPE.CONSUMABLE |
+        ITEM_TYPE.IS_TOOLBAR,
+    data: {
+        event: 'Plantsystem:FertilizePot',
+        amount: 25
+    }
+}
+const registerPlantFertilizer: Item = deepCloneObject<Item>(plantFertilizerItem);
+appendToItemRegistry(registerPlantFertilizer);
+
+const plantWaterItem: Item = {
+    name: 'Plantwater',
+    description: 'DEBUGItem.. Change me - I am used to water WeedPots.',
+    icon: 'crate',
+    slot: 0,
+    quantity: 25,
+    behavior:
+        ITEM_TYPE.CAN_DROP |
+        ITEM_TYPE.CAN_STACK |
+        ITEM_TYPE.CONSUMABLE |
+        ITEM_TYPE.IS_TOOLBAR,
+    data: {
+        event: 'Plantsystem:WaterPot',
+        amount: 25
+    }
+}
+const registerPlantWater: Item = deepCloneObject<Item>(plantWaterItem);
+appendToItemRegistry(registerPlantWater);
+
